8614bba698e7e9dcc7d9277f487bf9f731f3e38b
M-Court <mcourt@students.unwsp.edu>
2024-10-29 21:23:24 -0500

    Update helpweek9.py

---
+++ helpweek9.py
@@ -0,0 +1,26 @@
+#this program displays the contents of a file
+
+def main():
+	#get the name of a file.
+	userfilename = input("Enter a filename: ")
+	
+	try:
+		#open the file.
+		insidefile = open(userfilename, "r")
+	
+		#read the contnts
+		contents_of_file = insidefile.read()
+			
+		#display the file's contents
+		print(contents_of_file)
+			
+		#close the file
+		insidefile.close()
+			
+	except IOError:
+		print('An error occured trying to read')
+		print('the file', userfilename)
+
+#call the main function
+if _ _name_ _ == '_ _main_ _':
+main()
