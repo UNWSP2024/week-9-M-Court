f65d751e0e0b0b2a47ab27a5d6cf6525a43241e6
M-Court <mcourt@students.unwsp.edu>
2024-11-04 20:44:12 -0600

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,26 +1,36 @@
-# Program #3: Average Numbers
-# Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
-# (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+#3: Average Numbers
 
-# The program should handle the following exceptions: 
+#Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk. 
 
-# It should handle any IOError exceptions that are raised.
-# It should handle any ValueError exceptions that are raised when the items that are read from the file 
-# are converted to a number.
+#Write a program that 
+		#reads all of the numbers stored in the file 
+		
+		#and calculates their total.  
+
+#The program should handle the following exceptions: 
+
+	#It should handle any IOError exceptions that are raised.
+
+	#It should handle any ValueError exceptions that are raised when the items that are read from the file are converted to a number.
+	
+
+#read all numbers
+
+#set total at 0
+number_total = 0
+number = 0
 
 #read open file
 try:
 		with open("numbers.txt", "r") as open_numbers_file:
-				#read numbers
-				number = int(open_numbers_file.readline())
-				#make list
-				number_total = []
-				#add to total
-				number_total + number
-		
+				for number in open_numbers_file:
+					#read numbers
+					print(number)
+					#add to total
+					number_total += int(number)
+			
 		#display total
-		print(number_total)
+		print(f"total: {number_total}")
 
 #IOError 
 except IOError:
@@ -30,7 +40,4 @@ except IOError:
 except ValueError:
 		print("non-numeric data found in the file.")
 
-#other errors
-#except:
-	#	print("An error occured.")
 
